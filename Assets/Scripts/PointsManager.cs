0308b3f8585fa5ff2df2dcfbd9cef1a49b54aef9
Giulio <giuliobilli2001@gmail.com>
2023-03-28 13:26:38 +0200

    Added end game slot status visualization

--- Assets/Scripts/PointsManager.cs
+++ Assets/Scripts/PointsManager.cs
@@ -2,30 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SlotStatus {
+    Win,
+    Lose,
+    Draw,
+    Undefined,
+}
+
 public class PointsManager : MonoBehaviour
 {  
     [SerializeField]
     private SlotManager slotManager;
-    public int firstPlayerPoints;
-    public int secondPlayerPoints;
+    
+    [SerializeField]
+    private PointsUIManager pointsUIManager;
+    private List<SlotStatus> firstPlayerPoints = new List<SlotStatus>();
+    private List<SlotStatus> secondPlayerPoints = new List<SlotStatus>();
 
-    // Update is called once per frame
+    void Awake() {
+        for (int i=0; i<3; i++) {
+            firstPlayerPoints.Add(SlotStatus.Undefined);
+            secondPlayerPoints.Add(SlotStatus.Undefined);
+        }
+    }
     void Update()
     {
         
     }
 
     public void UpdatePlayersPoints() {
-        for(int position=3; position<=5; position++) {
-            CardType player1Slot = slotManager.firstPlayerSlots[position].GetCardType();
-            CardType player2Slot = slotManager.secondPlayerSlots[position].GetCardType();
+        for(int position=0; position<3; position++) {
+            CardType player1Slot = slotManager.firstPlayerSlots[position + 3].GetCardType();
+            CardType player2Slot = slotManager.secondPlayerSlots[position + 3].GetCardType();
 
             if ((player1Slot != player2Slot)) {
                 if (IsTheWinner(player1Slot, player2Slot)) {
-                    this.firstPlayerPoints++;
+
+                    this.firstPlayerPoints[position] = SlotStatus.Win;
+                    this.secondPlayerPoints[position] = SlotStatus.Lose;
+                    pointsUIManager.SetTextByIndex(position, SlotStatus.Win);
                 }else {
-                    this.secondPlayerPoints++;
+
+                    this.secondPlayerPoints[position] = SlotStatus.Win;
+                    this.firstPlayerPoints[position] = SlotStatus.Lose;
+                    pointsUIManager.SetTextByIndex(position, SlotStatus.Lose);
                 }
+            } else {
+            
+                    this.firstPlayerPoints[position] = SlotStatus.Draw;
+                    this.secondPlayerPoints[position] = SlotStatus.Draw;
+                    pointsUIManager.SetTextByIndex(position, SlotStatus.Draw);
             }
         }
     }
