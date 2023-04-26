df143077c3ba63008d6fbfcb58ee8f464f9997f9
Francesca Guzzi <76923530+francescaguzzi@users.noreply.github.com>
2023-04-26 18:01:40 +0200

    Fixed InputSystem for touchscreen

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,12 +1,10 @@
 using System.Collections;
 using UnityEngine;
+//using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
-using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
-public class DragAndDrop : MonoBehaviour
-
-{
-    [SerializeField] private InputAction inputAction;
+public class DragAndDrop : MonoBehaviour {
+    
     [SerializeField] private float smoothTime = 0.1f;
     private Vector3 velocity = Vector3.zero;
 
@@ -18,109 +16,102 @@ public class DragAndDrop : MonoBehaviour
 
     [SerializeField] private SlotManager slotManager;
 
-    private void Awake()
-    {
-        mainCamera = Camera.main;
-    }
+    private void Awake() {
+        this.mainCamera = Camera.main;
+    } 
 
-    private void OnEnable()
-    {
-        EnhancedTouchSupport.Enable();
+    private void OnEnable() {
         TouchSimulation.Enable();
-        Touch.onFingerDown += OnScreenTouch;
-        Touch.onFingerUp += OnFingerUp;
+        EnhancedTouchSupport.Enable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += OnTouch;
     }
 
-    private void OnDisable()
-    {
-        Touch.onFingerDown -= OnScreenTouch;
-        Touch.onFingerUp -= OnFingerUp;
-    }
+    private void OnDisable() {
+        TouchSimulation.Disable();
+        EnhancedTouchSupport.Disable();
+        UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= OnTouch;
+    } 
 
-    private void OnScreenTouch(Finger finger)
-    {
-        Ray ray = mainCamera.ScreenPointToRay(finger.screenPosition);
+    private void OnTouch(Finger finger) {
 
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            if (hit.collider != null)
-            {
+        Ray ray = this.mainCamera.ScreenPointToRay(finger.screenPosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit)) {
+            if (hit.collider != null) {
+
                 selectedObject = hit.collider.gameObject;
                 selectedSlot = selectedObject.GetComponent<Slot>();
-                StartCoroutine(MoveObject(selectedObject));
+                StartCoroutine(MoveObject(selectedObject, finger));
             }
         }
     }
 
-    private void OnFingerUp(Finger finger)
-    {
-        if (selectedObject != null)
-        {
-            CheckOverlappingSlots();
-            EndCardParallax(selectedObject);
-        }
-    }
+    // Coroutine for dragging the card
+    private IEnumerator MoveObject(GameObject selectedObject, Finger finger) {
 
-    private IEnumerator MoveObject(GameObject selectedObject)
-    {
-        float initialDistance = Vector3.Distance(selectedObject.transform.position, mainCamera.transform.position);
+        float initialDistance = Vector3.Distance(selectedObject.transform.position, this.mainCamera.transform.position);
 
-        while (Touch.activeFingers.Count > 0 &&
-               selectedObject != null &&
-               selectedSlot != null)
-        {
-            Ray ray = mainCamera.ScreenPointToRay(Touch.activeFingers[0].screenPosition);
+        while(finger.isActive) { // drag 
+        
+            Ray ray = this.mainCamera.ScreenPointToRay(finger.screenPosition);
             StartCardParallax(selectedObject);
             selectedObject.transform.position = Vector3.SmoothDamp(selectedObject.transform.position, ray.GetPoint(initialDistance), ref velocity, smoothTime);
             yield return waitForFixedUpdate;
         }
+        // drop
+        CheckOverlappingSlots(finger);
+        EndCardParallax(selectedObject);
+
     }
 
-    private void CheckOverlappingSlots()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+    // Check if the card is overlapping with another slot
+    private void CheckOverlappingSlots(Finger finger) {
+
+        Ray ray = Camera.main.ScreenPointToRay(finger.screenPosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
+        if (Physics.Raycast(ray, out hit)) {
+
             Slot overlapSlot = hit.collider.GetComponent<Slot>();
-            if (overlapSlot != null && overlapSlot != selectedSlot)
-            {
-                if (slotManager.GetCurrentPlayer(selectedSlot, overlapSlot) == 0)
-                {
+            if (overlapSlot != null && overlapSlot != selectedSlot) {
+                // avoiding cards overlapping opposite player's cards
+                if (slotManager.GetCurrentPlayer(selectedSlot, overlapSlot) == 0) {
                     SnapBack();
-                }
-                else
-                {
+                } else {
                     slotManager.Swap(selectedSlot, overlapSlot);
                 }
-            }
-            else
-            {
+    
+            } else {
+                // snapping back to previous position
                 SnapBack();
             }
         }
     }
 
-    private void StartCardParallax(GameObject selectedObject)
-    {
+    // Parallax feature
+    // when a card is being dragged, it rotates on itself
+    private void StartCardParallax(GameObject selectedObject) {
+        
         float maxRotationAngle = 10f;
         float rotationSpeed = 2f;
+
         float additionalRotation = Mathf.Sin(Time.time * rotationSpeed) * maxRotationAngle;
         Quaternion rotation = Quaternion.Euler(0, additionalRotation, 0);
+
         selectedObject.transform.rotation = rotation;
     }
 
-    private void EndCardParallax(GameObject selectedObject)
-    {
+    private void EndCardParallax(GameObject selectedObject) {
         selectedObject.transform.rotation = Quaternion.identity;
     }
 
-    private void SnapBack()
-    {
+    private void SnapBack() {
+        
         Vector3 snapPosition = selectedSlot.GetCurrentPosition();
         if (selectedObject != null && selectedSlot != null && Vector3.Distance(selectedObject.transform.position, snapPosition) > 0.5f) {
             iTween.MoveTo(selectedObject, iTween.Hash("position", snapPosition, "time", 0.2f, "easetype", iTween.EaseType.easeOutBack));
         }
+
     }
 
 
